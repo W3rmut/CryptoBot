4335da25f0a4bf422f9ffbbf04b8b1d55cf9460a
W3rmut <wermut.bm@gmail.com>
2021-10-01 17:29:01 +0300

    BTC ETH added

---
+++ internal/crypto/actions.go
@@ -0,0 +1,15 @@
+package bittrexSDK
+
+const apiURL = "https://api.bittrex.com/api/v1.1/public/getticker?market="
+
+func GetXMR(){
+	url := apiURL + ""
+}
+
+func GetBTC(){
+
+}
+
+func GetETH() {
+
+}
